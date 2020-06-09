5de05267d2bf8e3adece74af9d495a3a9b732997
QMeier <quinnmeier3@gmail.com>
2020-06-09 17:02:13 -0500

    added new files and updated first exercise.

--- Ruby/guess_the_number.rb
+++ Ruby/guess_the_number.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
-begin
-  done = false
-  until done do
+def play
+  $done = false
+  until $done do
       x = rand(11)
       guess = false
       puts "Guess a number between 0 and 10, inclusive. \n"
@@ -12,17 +12,24 @@ begin
         puts 'Your guess is too high. Try again!' if y > x
         puts 'Your guess is too low. Try again!' if y < x
         if y == x
-          puts "You got it! \n"
+          puts "You got it! \n\n"
           guess = true
         end
       end
-
-      puts 'Would you like to play again? (0/1)'
-      tmp = gets
-      if (tmp.to_i == 0)
-        done = true
-        puts "\n\n"
-      end
+      play_again()
   end
+end
+
+def play_again
+  puts 'Would you like to play again? (0/1)'
+  tmp = gets
+  if (tmp.to_i == 0)
+    $done = true
+    puts "\n\n"
+  end
+end
+
+begin
+  play()
   puts "Thanks for playing! \n\n"
 end
