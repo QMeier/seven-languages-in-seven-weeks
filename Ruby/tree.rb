2dfe5872aedb17a52c49a3325be426b7422d8e45
QMeier <quinnmeier3@gmail.com>
2020-06-12 18:15:10 -0500

    update tree.rb

--- Ruby/tree.rb
+++ Ruby/tree.rb
@@ -2,10 +2,13 @@
 class Tree
   attr_accessor :children, :node_name
 
-  def initialize(name, children = [])
-    @children = children
+  def initialize(name, val = {})
+    @children = []
     @node_name = name
-  end
+    if(val.is_a?(Hash))
+      val.keys.each do |key|
+        children.push Tree.new(key, val[key])
+      end
 
   def visit_all(&block)
     vist &block
@@ -27,45 +30,3 @@ begin
   puts "visiting entire tree"
   ruby_tree.visit_all {|node| puts node.node_name}9
 end
-
-
-
-class Tree
-  attr_accessor :children, :node_name
-
-  def initialize(name, val={})
-    @node_name = name
-    @children = []
-    if (val.is_a?(Hash))
-      val.keys.each do |key|
-        children.push Tree.new(key, val[key])
-      end
-    elsif (val)
-      children.push(Tree.new(val, nil))
-    end
-  end
-
-  def visit_all(&block)
-    visit &block
-    children.each {|c| c.visit_all &block}
-  end
-
-  def visit(&block)
-    block.call self
-  end
-end
-
-# ruby_tree = Tree.new("Ruby", [Tree.new("Reia"), Tree.new("MacRuby")])
-ruby_tree = Tree.new("Ruby", {
-  "raby": 34,
-  "rewad": {
-    "342": "test"
-    }
-  })
-
-puts "visiting a node"
-  ruby_tree.visit {|node| puts node.node_name}
-puts
-
-puts "visiting entire tree"
-  ruby_tree.visit_all {|node| puts node.node_name}
